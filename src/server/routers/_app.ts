8157fd6b11ea4185e1d69913f67b86df3dbd068c
zivavu <zivavu@gmail.com>
2025-02-12 19:01:53 +0100

    :wastebasket: Remove unused server utils

--- src/server/routers/_app.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from '../trpc';
-import { userRouter } from './userRouter';
-
-export const appRouter = createTRPCRouter({
-  userRouter,
-});
-
-export type AppRouter = typeof appRouter;
