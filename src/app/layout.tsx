556549201b5f2c47a830ab6530179af28c65d542
unknown <zivavu@gmail.com>
2025-03-02 10:39:04 +0100

    :arrow_up: Update the deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { FetchingLayerProvider } from '@/lib/FetchingLayerProvider';
 import type { Metadata } from 'next';
 import { ThemeProvider } from 'next-themes';
-import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
 
 import { Footer } from '@/features/global/components/Footer';
@@ -12,16 +12,6 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
 export const metadata: Metadata = {
   title: 'Tiny Gems',
   description: 'A cozy corner of the internet for artists to share their music, drawings, and handcrafted creations.',
@@ -30,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
+      <body className="antialiased min-h-screen flex flex-col">
         <ThemeProvider defaultTheme="system" enableSystem enableColorScheme disableTransitionOnChange attribute="class">
           <FetchingLayerProvider>
             <Toaster />
